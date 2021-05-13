de5c78a4f834ad49f5733163cc19155f5dbb3dae
Rebecca DeBenedictis <rjdebened@verizon.net>
2021-05-13 18:23:43 -0400

    Initial Commit

---
+++ To-do list for training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-do list for training
+//
+//  Created by Rebecca DeBenedictis on 5/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
